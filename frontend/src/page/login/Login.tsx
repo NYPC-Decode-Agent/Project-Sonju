8f1a283614711387b231fe4d457a6a2513ac99a4
rlazoxl <aidenkim0826@gmail.com>
2024-08-17 01:57:48 +0900

    feat: api 물리기 완료

--- frontend/src/page/login/Login.tsx
+++ frontend/src/page/login/Login.tsx
@@ -1,17 +1,38 @@
-import React, { useState } from 'react';
-import { Button, TextField, Typography, Container, Paper } from '@mui/material';
-import { useNavigate } from 'react-router-dom'; // 최신 라우팅 훅
+import React, { useState } from "react";
+import { Button, TextField, Typography, Container, Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom"; // 최신 라우팅 훅
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
 // 로그인 폼 데이터 타입 정의
 interface LoginFormData {
-  email: string;
+  phone: string;
   password: string;
 }
 
 const Login: React.FC = () => {
-  const [formData, setFormData] = useState<LoginFormData>({ email: '', password: '' });
+  const [formData, setFormData] = useState<LoginFormData>({
+    phone: "",
+    password: "",
+  });
   const [error, setError] = useState<string | null>(null); // 에러 상태
   const navigate = useNavigate(); // 페이지 전환에 사용
+
+  const { mutate: postSignIn } = useMutation({
+    mutationFn: ({ phone, password }: { phone: string; password: string }) =>
+      axios.post("http://localhost:4000/api/user/sign-in", {
+        userInfo: {
+          phone,
+          password,
+        },
+      }),
+    onSuccess: () => {
+      console.log("success");
+    },
+    onError: () => {
+      console.log("error");
+    },
+  });
 
   // 입력값 변경 핸들러
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,20 +43,8 @@ const Login: React.FC = () => {
   // 폼 제출 핸들러
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    try {
-      // 여기에 로그인 API 요청을 넣습니다.
-      // 예시: await api.login(formData.email, formData.password);
-
-      // 성공 시 리다이렉트
-      console.log('[formData : ]',formData)
-      navigate('/main'); // 로그인 성공 후 대시보드 페이지로 이동
-
-      // 실제 환경에서는 성공과 실패에 대한 처리가 필요합니다.
-      // 성공 시 리다이렉트하고, 실패 시 에러 메시지를 설정합니다.
-    } catch (err) {
-      setError('로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.');
-    }
+    postSignIn({ phone: formData.phone, password: formData.password });
+    console.log(` sign in ${formData.phone} ${formData.password}`);
   };
 
   return (
@@ -48,9 +57,9 @@ const Login: React.FC = () => {
             margin="normal"
             required
             fullWidth
-            label="이메일"
-            name="email"
-            value={formData.email}
+            label="전화번호"
+            name="phone"
+            value={formData.phone}
             onChange={handleChange}
             autoFocus
           />
