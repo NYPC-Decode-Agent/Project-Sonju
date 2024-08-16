9ce9aac8fa6bd612ef076593a48cea23a797fc2b
rlazoxl <aidenkim0826@gmail.com>
2024-08-16 23:13:34 +0900

    feat: main으로 통일

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,8 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import routes from "./router/routes";
-import { Sidebar } from "./components/Sidebar";
+import React from "react";
+import { Main } from "./page/main";
 
 const App: React.FC = () => {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
-  return (
-    <Router>
-      <div className="App">
-        <Header toggleSidebar={toggleSidebar} />
-        <Sidebar
-          isOpen={isSidebarOpen}
-          closeSidebar={() => setSidebarOpen(false)}
-        />
-        <header className="App-header app-container"></header>
-        <main>
-          <Routes>
-            {routes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element} />
-            ))}
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
+  return <Main />;
 };
 
 export default App;
--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -1,23 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Button } from "../../common/button";
-import { Input } from "../../common/Input";
-import { useState } from "react";
-import { Footer } from "../../common/Footer";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import routes from "../../router/routes";
+import { Footer } from "../../common/footer";
 
 export const Main = () => {
   const [value, setValue] = useState("김철수");
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Input onChange={setValue} value={value} label="이름" />
-        <Button onClick={() => {}}>Learn React</Button>
-      </header>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header toggleSidebar={toggleSidebar} />
+        <Sidebar
+          isOpen={isSidebarOpen}
+          closeSidebar={() => setSidebarOpen(false)}
+        />
+        <header className="App-header app-container"></header>
+        <main>
+          <Routes>
+            {routes.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 };
