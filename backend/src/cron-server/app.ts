859ea88a8a59dd8f98fb610504d14a60880ecf8a
skykhs3 <skykhs3@naver.com>
2024-08-17 02:29:54 +0900

    feat: alarm route

--- backend/src/cron-server/app.ts
+++ backend/src/cron-server/app.ts
@@ -12,7 +12,7 @@ db.raw("SELECT 1")
     // 크론 작업 정의 및 시작
     const job = new CronJob(
       "0 * * * * *", // cronTime
-      callScheduleJob.checkCallSchedule, // onTick
+      callScheduleJob.checkValidAlarm, // onTick
       null, // onComplete
       true, // start
       config.CRON_SERVER.TIME_ZONE // timeZone
