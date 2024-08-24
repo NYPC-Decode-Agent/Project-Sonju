874200609a272b116af5de4db2bb4d11c672ce83
cgiosy <cgiosy0@gmail.com>
2024-08-24 18:24:26 +0900

    feat: 페이지 컨테이너 컴포넌트 추가

---
+++ frontend/src/common/Container/index.tsx
@@ -0,0 +1,23 @@
+type MainProps = {
+  children: React.ReactNode;
+};
+
+type SmallPageProps = {
+  children: React.ReactNode;
+  header: string;
+};
+
+export const Main = ({ children }: MainProps) => (
+  <main className="relative flex flex-col min-h-[calc(100vh_-_7.5rem)] w-full items-center justify-center">
+    {children}
+  </main>
+);
+
+export const SmallPage = ({ children, header }: SmallPageProps) => (
+  <div className="bg-white max-w-xl w-full px-16 pb-20 mt-12 rounded-xl border border-gray-lighter shadow-light-md">
+    <h2 className="flex justify-center p-6 border-b border-gray-extra-light my-10 text-3xl font-bold">
+      {header}
+    </h2>
+    {children}
+  </div>
+);
