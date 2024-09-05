5057dd67f7d5cc311dbe4b3bed45ddd7ed83adbc
cgiosy <cgiosy0@gmail.com>
2024-09-06 08:50:29 +0900

    fix: TextField 폰트 따라 위치 안 맞는 문제 수정

--- frontend/src/common/TextField/index.tsx
+++ frontend/src/common/TextField/index.tsx
@@ -16,7 +16,7 @@ const textInputCss =
   "peer block contain-size w-full bg-white caret-primary-650 border-none leading-5 focus:outline-none";
 
 const textPlaceholderCss =
-  "absolute top-[calc(50%_-_8px)] left-3 px-2 leading-4 text-gray-light bg-white [transition:color_.2s,_transform_.2s] origin-top-left select-none cursor-text peer-focus:text-primary-600 peer-focus:translate-y-[calc(-50%_-_18px)] peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:translate-y-[calc(-50%_-_18px)] peer-focus:scale-75";
+  "absolute top-[18px] left-3 px-2 leading-4 text-gray-light bg-white [transition:color_.2s,_transform_.2s] origin-top-left select-none cursor-text peer-focus:text-primary-600 peer-focus:translate-y-[calc(-50%_-_18px)] peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:translate-y-[calc(-50%_-_18px)] peer-focus:scale-75";
 
 export const TextField = ({ placeholder, ...props }: InputProps) => (
   <label className={textLabelCss}>
